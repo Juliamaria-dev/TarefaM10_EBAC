b128f7958e4c74df3f33acf90240ee3bccd16f58
Júlia Maria Dev <133073660+Juliamaria-dev@users.noreply.github.com>
2024-07-27 04:20:32 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    $('#telefone').mask('(00) 00000-0000', { placeholder: '(DDD) 12345-6789' });
+    $('#cpf').mask('000.000.000-00', { placeholder: '123.456.789-00' });
+    $('#cep').mask('00000-000', { placeholder: '012345-678' });
+
+    $('#form').validate({
+        rules: {
+            nome: { required: true },
+            email: { required: true, email: true },
+            telefone: { required: true },
+            endereco: { required: true },
+            cep: { required: true },
+            cpf: { required: true }
+        },
+        submitHandler: function (form) {
+            alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
+            form.reset();
+        },
+        invalidHandler: function (event, validator) {
+            alert("Por favor, preencha os campos para prosseguir com a compra!");
+        }
+    });
+
+    $('#carousel-imagens').slick({
+        autoplay: true,
+        arrows: false,
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    });
+});
